b12643fc4013c915048db15f370a8948237f74f0
malditoprotozoo <virodriguez.fe@gmail.com>
2017-12-13 12:55:57 -0300

    Restaurants Data

---
+++ assets/js/data.js
@@ -0,0 +1,45 @@
+var restaurants = [
+  {
+    name: "Runaway Sushi",
+    address: ["Rancagua 574, Providencia, Santiago"],
+    filters: ["vegan", "vegetarian", "sushi", "japanese", "asian", "delivery"],
+    photo: "<img src='assets/img/runaway-sushi.jpg' alt='Runaway Sushi' class='col s12 l6 xl6'>",
+    website: "http://runawaysushi.cl"
+  },
+  {
+    name: "Take a Wok",
+    address: ["Local 5112, Andrés Bello 2447, Santiago"],
+    filters: ["vegan", "vegetarian", "asian", "thai"],
+    photo: "<img src='assets/img/take-a-wok.jpg' alt='Take a Wok' class='col s12 l6 xl6'>",
+    website: "http://takeawok.cl/"
+  },
+  {
+    name: "Wok",
+    address: ["Patio de Comidas, Andrés Bello 2447, Santiago",
+    "Patio de Comidas, Presidente Kennedy Lateral 9001, Las Condes, Santiago"],
+    filters: ["vegan", "vegetarian", "asian", "thai"],
+    photo: "<img src='assets/img/wok.jpeg' alt='Wok' class='col s12 l6 xl6'>",
+    website: "http://www.wokchile.com/"
+  },
+  {
+    name: "Shakti",
+    address: ["Italia 1568, Ñuñoa, Santiago"],
+    filters: ["vegan", "pizza", "mediterranean"],
+    photo: "<img src='assets/img/shakti.jpg' alt='Shakti' class='col s12 l6 xl6'>",
+    website: "https://www.facebook.com/shaktiveganochile/"
+  },
+  {
+    name: "Vegan Bunker",
+    address: ["Fresia 529, Providencia, Santiago"],
+    filters: ["vegan", "pizza", "fastfood", "bakery"],
+    photo: "<img src='assets/img/vegan-bunker.jpg' alt='Vegan Bunker' class='col s12 l6 xl6'>",
+    website: "https://www.facebook.com/VeganBunker/"
+  },
+  {
+    name: "Central Friends",
+    address: ["Local A, Los Militares 5890, Las Condes, Santiago"],
+    filters: ["coffee", "bakery"],
+    photo: "<img src='assets/img/central-friends.jpg' alt='Central Friends' class='col s12 l6 xl6'>",
+    website: "http://www.centralfriends.cl/"
+  }
+]
