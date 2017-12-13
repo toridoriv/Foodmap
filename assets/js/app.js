2ed4f758341642dfbebb27eef298eab4742c4625
malditoprotozoo <virodriguez.fe@gmail.com>
2017-12-13 12:56:50 -0300

    Functions added, Loader deactivated temporarily

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,34 @@
 $(document).ready(function() {
+  /**
+  * Function that makes the loader fadeout after 6 seconds
+  */
   setTimeout(function(){
     $("#loader").fadeOut();
   },6000);
+  /**
+  * Makes the Materialize Select to appears when the loader dissapears
+  */
+  setTimeout(function(){
+    $("select").material_select();
+  },0);
+  /**
+  * Makes the images show when the value of a selection matches the filters of the restaurant
+  */
+  $("select").change(function() {
+    $("#restaurants-container").children().remove();
+    var selection = $("select").val();
+    for (var i = 0; i < restaurants.length; i++) {
+      for(var n = 0; n < restaurants[i].filters.length; n++) {
+        if (restaurants[i].filters[n] == selection) {
+          $("#restaurants-container").append(restaurants[i].photo);
+        }
+      }
+    }
+  });
 });
+
+
+
+// $("select").val()
+// if ($("select").val() == "vegan") {
+// $("#restaurants-container").append(restaurants[0].photo);
