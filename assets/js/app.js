108964c3132554179d5ee5157813b0a8ebbcf859
malditoprotozoo <virodriguez.fe@gmail.com>
2017-12-14 19:06:30 -0300

    Google Map added and functional. It's alive, people <3

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,6 +12,69 @@ var addFilters = (function(element) {
   return filtersFinal;
 });
 
+
+
+var myLocation;
+
+var initMap = (function() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 14
+  });
+  var infoWindow = new google.maps.InfoWindow({map: map});
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+    };
+    myLocation = pos;
+    infoWindow.setPosition(pos);
+    infoWindow.setContent('Location found.');
+    var thisPlace = new google.maps.LatLng(myLocation.lat,myLocation.lng);
+    var service = new google.maps.places.PlacesService(map);
+    service.nearbySearch({
+      location: thisPlace,
+      radius: 1500,
+      type: ["food"]
+    }, callback);
+    function callback(results, status) {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+          createMarker(results[i]);
+        }
+      }
+    };
+    function createMarker(thisPlace) {
+      var placeLoc = thisPlace.geometry.location;
+      var marker = new google.maps.Marker({
+        map: map,
+        position: thisPlace.geometry.location
+      });
+      google.maps.event.addListener(marker, 'click', function() {
+        infoWindow.setContent(thisPlace.name);
+        infoWindow.open(map, this);
+      });
+    };
+
+    map.setCenter(pos);
+  }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+      handleLocationError(false, infoWindow, map.getCenter());
+  }
+});
+
+var handleLocationError = (function(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
+});
+
+  //var thisPlace = new google.maps.LatLng(myLocation.lat,myLocation.lng);
+
 $(document).ready(function() {
   /**
   * Function that creates new options according to the tags
@@ -23,6 +86,12 @@ $(document).ready(function() {
   setTimeout(function(){
     $("#loader").fadeOut();
   },6000);
+  setTimeout(function() {
+    initMap();
+  },6000);
+  // setTimeout(function() {
+  //   initialize();
+  // },9000);
   /**
   * Initializes the modal
   */
@@ -78,17 +147,8 @@ $(document).ready(function() {
       */
       $(".modal-overlay").click(function() {
         $("#restaurants-container").children().remove();
-        $("select").val(null);
+        $("#filter").val(null);
       })
     });
   });
 });
-
-
-
-// AIzaSyAR26jcQ0wriBfIDM3j327c80TqkZjw8-A
-
-/* "<iframe src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAR26jcQ0wriBfIDM3j327c80TqkZjw8-A&q=Space+Needle,Seattle+WA" allowfullscreen></iframe> */
-
-// if ($("select").val() == "vegan") {
-// $("#restaurants-container").append(restaurants[0].photo);
